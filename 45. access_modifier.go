02a1b361476795b6270aee75d4c025eb9fa1b477
Arya Sukma Wijaya <aryasukma.work@gmail.com>
2020-10-04 16:22:36 +0700

    #5 update

--- 45. access_modifier.go
+++ 45. access_modifier.go
@@ -1,13 +1,14 @@
 package main
 
+// jika melakukan import namun tidak digunakan, agar tidak terjadi error maka bisa menambahkan blank identifier (_) sebelum nama package
 import (
-	"belajar-golang-dasar/helper"
+	_ "belajar-golang-dasar/helper"
 )
 
 func main() {
-	helper.SayHello("arya")
+	// helper.SayHello("arya")
 
 	// error, karena di helper diawali oleh huruf kecil
 	// cannot refer to unexported name helper.sayGoodBye
-	helper.sayGoodBye()
+	// helper.sayGoodBye()
 }
